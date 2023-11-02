48e3dbccc1de5f76bc90dec9fa9ea7378269745d
hedz <hedzy@yandex.ru>
2023-11-02 15:43:54 +0300

    first commit

---
+++ ConsoleApplication64.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    int vvod = 0;
+    do {
+        std::cout << "Hello mir?[1/2]";
+        std::cin >> vvod;
+        if (vvod == 1) { std::cout << "xxxmanera krutit mir"; break; };
+        if (vvod == 2) { std::cout << "Jalko"; break; };
+    } while (vvod != 0);
+}
